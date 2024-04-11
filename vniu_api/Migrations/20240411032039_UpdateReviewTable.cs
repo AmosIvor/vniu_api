f6a6a6c8db1668c06d9143e11b06f114a2ad2844
AmosIvor <trantuanvu00000@gmail.com>
2024-04-11 11:57:31 +0700

    [REVIEW] add api and logic CRUD

---
+++ vniu_api/Migrations/20240411032039_UpdateReviewTable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace vniu_api.Migrations
+{
+    public partial class UpdateReviewTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ReviewCreateAt",
+                table: "Review",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ReviewUpdateAt",
+                table: "Review",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReviewCreateAt",
+                table: "Review");
+
+            migrationBuilder.DropColumn(
+                name: "ReviewUpdateAt",
+                table: "Review");
+        }
+    }
+}
--- vniu_api/Repositories/Reviews/IReviewRepo.cs
+++ vniu_api/Repositories/Reviews/IReviewRepo.cs
@@ -1,6 +1,17 @@
-﻿namespace vniu_api.Repositories.Reviews
+﻿
+using vniu_api.ViewModels.ReviewsViewModels;
+
+namespace vniu_api.Repositories.Reviews
 {
     public interface IReviewRepo
     {
+        Task<ICollection<ReviewVM>> GetReviewsAsync();
+        Task<ReviewVM> GetReviewByIdAsync(int reviewId);
+        Task<ICollection<ReviewVM>> GetReviewByUserIdAsync(string userId);
+        Task<ICollection<ReviewVM>> GetReviewByProductItemIdAsync(int productItemId);
+        Task<ReviewVM> CreateReviewAsync(ReviewVM reviewVM);
+        Task<ReviewVM> UpdateReviewAsync(int reviewId, ReviewVM reviewVM);
+        Task<ReviewVM> DeleteReviewAsync(int reviewId);
+        Task<bool> IsReviewExistIdAsync(int reviewId);
     }
 }
